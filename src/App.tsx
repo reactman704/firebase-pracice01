64bd3f73488592d454bc2a6bf21332a62463cecf
Greatteacherjubro <wisdomjh100@gmail.com>
2025-07-23 17:17:32 +0900

    feat: NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import { DetailPage } from './pages/DetailPage'
+import { LoginPage } from './pages/LoginPage'
+import { NavBar } from './components/NavBar'
 
+
+const Layout = () => {
+  return(
+    <>
+      <NavBar />
+      <br />
+      <br />
+      <br />
+      <Outlet />
+    </>
+  )
+}
 
 function App() {
   
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<MainPage/>} />
-          <Route path='/DetailPage' element={<DetailPage/>} />
-
+          <Route path='/' element={<Layout />}>
+            <Route index path='/' element={<MainPage/>} />
+            <Route path='/login' element={<LoginPage/>} />
+            <Route path='/detail' element={<DetailPage/>} />
+          </Route>
         </Routes>
       </BrowserRouter>    
     </>
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,13 +2,27 @@ import { Link } from "react-router-dom"
 
 
 
+
 function MainPage() {
   
+    const Header = () => (
+    <div className="header">
+      <div>
+        <Link to="/login">ddd</Link>
+      </div>
+      <div>
+        <Link to="/detail">2222</Link>
+      </div>
+    </div>
+  );
 
   return (
     <>
+      <Header></Header>
+
       <div>여기는 메인페이지 입니당</div>
-      <Link to="/DetailPage">ddd</Link>
+
+      
     </>
   )
 }
